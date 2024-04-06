6b26b2a185796ad9bef42006b3ef5896e9410b7d
Abhijit Salunkhe <79576125+abhisalunkhe@users.noreply.github.com>
2024-04-06 23:13:33 +0530

    Add files via upload

---
+++ scripte.js
@@ -0,0 +1,7 @@
+document.getElementById('createEsignBtn').addEventListener('click', function() {
+    window.location.href = './esign.html';
+  });
+
+  document.getElementById('imageResizeBtn').addEventListener('click', function() {
+    window.location.href = './imageResizer.html';
+  });
